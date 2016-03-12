991ba382b988c47232951f2edb1a253c2f53f76c
simagle <simagle@163.com>
2016-03-12 22:23:32 +0800

    工程项目构建

---
+++ cm-service/src/main/java/cn/eatammy/dao/ICMBaseDAO.java
@@ -0,0 +1,10 @@
+package cn.eatammy.dao;
+
+import cn.eatammy.common.dao.IBaseDAO;
+import cn.eatammy.common.domain.BaseDomain;
+
+/**
+ * Created by 郭旭辉 on 2016/3/10.
+ */
+public interface ICMBaseDAO<T extends BaseDomain> extends IBaseDAO<T> {
+}
