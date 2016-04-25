692e362bc11aabde30c59115f2335ace8889e09d
guoxuhui <simagle@163.com>
2016-04-25 23:12:18 +0800

    用户控制类

--- cm-admin-war/src/main/java/cn/eatammy/cm/controller/user/UserController.java
+++ cm-admin-war/src/main/java/cn/eatammy/cm/controller/user/UserController.java
@@ -2,6 +2,7 @@ package cn.eatammy.cm.controller.user;
 
 import cn.eatammy.cm.domain.user.UserDetail;
 import cn.eatammy.cm.service.user.IUserDetailService;
+import cn.eatammy.common.sms.SMSTYPE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,9 +38,29 @@ public class UserController {
         return userDetailService.isLogin(username, password, session, response);
     }
 
+    /**
+     * 注销
+     * @param session session
+     * @return 返回，操作码
+     */
     @ResponseBody
     @RequestMapping(value = "/logout")
     public String logout(HttpSession session){
         return userDetailService.logout(session);
     }
+
+    /**
+     * 找回密码
+     * @param username      用户名
+     * @param password      密码
+     * @param verifiedCode  验证码
+     * @return 返回，操作码
+     */
+    @ResponseBody
+    @RequestMapping(value = "/forgetPassword")
+    public String forgetPassword(String username, String password, String verifiedCode){
+        return userDetailService.forgetPasswd(username, password,verifiedCode, SMSTYPE.FORGET_PASSWD_SMS.getTypeValue());
+    }
+
+
 }
